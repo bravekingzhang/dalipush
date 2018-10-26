cda7f8f7ca22ee53e1d199573b0139e6ac37b13e
junshao <joshuaton@gmail.com>
2018-10-26 22:15:45 +0800

    【新增】阿里消息

--- ios/Classes/DalipushPlugin.m
+++ ios/Classes/DalipushPlugin.m
@@ -1,4 +1,6 @@
 #import "DalipushPlugin.h"
+#import <CloudPushSDK/CloudPushSDK.h>
+#import "MJExtension.h"
 
 @interface DalipushPlugin() <FlutterStreamHandler>
 
@@ -8,12 +10,19 @@
 
 @implementation DalipushPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+    
+    
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"dalipush"
             binaryMessenger:[registrar messenger]];
-    FlutterEventChannel *eventChannel = [FlutterEventChannel eventChannelWithName:@"dalipush_event" binaryMessenger:[registrar messenger]];
   DalipushPlugin* instance = [[DalipushPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+    
+    FlutterEventChannel *eventChannel = [FlutterEventChannel eventChannelWithName:@"dalipush_event" binaryMessenger:[registrar messenger]];
+    [eventChannel setStreamHandler:instance];
+    
+    [instance registerMessageReceive];
+
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -28,11 +37,44 @@
 
 - (FlutterError* _Nullable)onListenWithArguments:(id _Nullable)arguments
                                        eventSink:(FlutterEventSink)eventSink{
-    self.eventSink = eventSink
+    self.eventSink = eventSink;
+    return nil;
 }
 
 - (FlutterError* _Nullable)onCancelWithArguments:(id _Nullable)arguments{
     return nil;
 }
+
+/**
+ *    注册推送消息到来监听
+ */
+- (void)registerMessageReceive {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onMessageReceived:)
+                                                 name:@"CCPDidReceiveMessageNotification"
+                                               object:nil];
+}
+/**
+ *    处理到来推送消息
+ *
+ *    @param     notification
+ */
+- (void)onMessageReceived:(NSNotification *)notification {
+    
+    NSLog(@"onMessageReceived userinfo : %@", notification.userInfo);
+    
+    CCPSysMessage *message = [notification object];
+    NSString *title = [[NSString alloc] initWithData:message.title encoding:NSUTF8StringEncoding];
+    NSString *body = [[NSString alloc] initWithData:message.body encoding:NSUTF8StringEncoding];
+    NSLog(@"Receive message title: %@, content: %@.", title, body);
+    
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+    dict[@"title"] = title;
+    dict[@"body"] = body;
+    NSString *jsonStr = [[dict mj_JSONString] copy];
+    self.eventSink(jsonStr);
+}
+
+
 
 @end
