8ba2836e420fe191a3693b6d76e9f9c35cccfe97
junshao <joshuaton@gmail.com>
2018-10-26 19:24:04 +0800

    【新增】阿里消息

--- ios/Classes/DalipushPlugin.m
+++ ios/Classes/DalipushPlugin.m
@@ -1,10 +1,17 @@
 #import "DalipushPlugin.h"
 
+@interface DalipushPlugin() <FlutterStreamHandler>
+
+@property (nonatomic, strong) FlutterEventSink eventSink;
+
+@end
+
 @implementation DalipushPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"dalipush"
             binaryMessenger:[registrar messenger]];
+    FlutterEventChannel *eventChannel = [FlutterEventChannel eventChannelWithName:@"dalipush_event" binaryMessenger:[registrar messenger]];
   DalipushPlugin* instance = [[DalipushPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
@@ -15,6 +22,17 @@
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+#pragma mark - FlutterStreamHandler
+
+- (FlutterError* _Nullable)onListenWithArguments:(id _Nullable)arguments
+                                       eventSink:(FlutterEventSink)eventSink{
+    self.eventSink = eventSink
+}
+
+- (FlutterError* _Nullable)onCancelWithArguments:(id _Nullable)arguments{
+    return nil;
 }
 
 @end
